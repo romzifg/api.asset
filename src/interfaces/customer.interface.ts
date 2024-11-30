bead99fc1aca3f1fca69eba885ccd0a860836b3b
dvsynergi <info@dvsynergi.com>
2024-11-30 22:32:49 +0700

    update: add customer interface

---
+++ src/interfaces/customer.interface.ts
@@ -0,0 +1,11 @@
+export interface CustomerInterface {
+    id: number
+    name: string
+    address: string
+    pic?: string
+    phone?: string
+    email?: string
+    status: number
+    created_at?: Date
+    updated_at?: Date
+}
